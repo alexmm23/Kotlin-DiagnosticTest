123545d286c97c2c11429d7c148e281af15063e0
alexmm23 <alejandromontes393@gmail.com>
2024-08-18 13:47:37 -0600

    Validate ids from sharedPreferences's objects

--- app/src/main/java/com/example/diagnosticomovil2/models/IdGenerator.kt
+++ app/src/main/java/com/example/diagnosticomovil2/models/IdGenerator.kt
@@ -1,8 +1,14 @@
 package com.example.diagnosticomovil2.models
+
+import android.content.Context
+
 object IdGenerator {
     private var currentId = 0
-
-    fun getNextId(): Int {
+    private var utils: Utils = Utils()
+    fun getNextId(context: Context): Int {
+        utils.getToolsFromSharedPreferences(context)?.let {
+            currentId = it.maxOf { tool -> tool.id }
+        }
         currentId++
         return currentId
     }
