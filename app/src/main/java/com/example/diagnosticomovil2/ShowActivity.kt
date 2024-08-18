7bc548720f34edd60b5b4ca74fb02bcd60352197
alexmm23 <alejandromontes393@gmail.com>
2024-08-18 13:04:31 -0600

    Add ShowActivity

--- app/src/main/java/com/example/diagnosticomovil2/ShowActivity.kt
+++ app/src/main/java/com/example/diagnosticomovil2/ShowActivity.kt
@@ -5,13 +5,34 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
+import com.example.diagnosticomovil2.models.Tool
+import com.example.diagnosticomovil2.models.Utils
 
 class ShowActivity : AppCompatActivity() {
+
+    val utils = Utils()
+    private lateinit var tools: List<Tool>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show)
+        tools = utils.getToolsFromSharedPreferences(this) ?: emptyList()
         val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar2)
         setSupportActionBar(toolbar)
+        val txtShow: TextView = findViewById(R.id.txtShow)
+        var text = ""
+        if (tools.isEmpty()) {
+            text = "No hay herramientas registradas"
+        } else {
+            text = "Herramientas registradas:\n"
+            text += "ID - Nombre - Tipo - Marca - Precio - Es eléctrica\n"
+            var isElectric = ""
+            for (tool in tools) {
+                isElectric = if (tool.isElectric) "Si" else "No"
+                text += "${tool.id} - ${tool.name} - ${tool.type} - ${tool.brand} - ${tool.price} - ${isElectric}\n"
+            }
+        }
+        txtShow.text = text
     }
 
     // Import menu
